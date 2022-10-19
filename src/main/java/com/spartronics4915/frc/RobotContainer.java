89479348e89b9ccf6cf7ccef14fc604fcaeb199b
ss0g <55930267+ss0g@users.noreply.github.com>
2022-10-18 20:06:50 -0700

    added functionality to ExampleSubsystem

--- src/main/java/com/spartronics4915/frc/RobotContainer.java
+++ src/main/java/com/spartronics4915/frc/RobotContainer.java
@@ -4,46 +4,71 @@
 
 package com.spartronics4915.frc;
 
-import com.spartronics4915.frc.commands.ExampleCommand;
+import com.spartronics4915.frc.commands.ExampleCommands;
 import com.spartronics4915.frc.subsystems.ExampleSubsystem;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 
+import static com.spartronics4915.frc.Constants.OIConstants.*;
+
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    private final Joystick mJoystick;
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+    // The robot's subsystems and commands are defined here...
+    private final ExampleSubsystem mExampleSubsystem;
+    private final ExampleCommands mExampleCommands;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+    private final Command mAutoCommand;
+    private final Command mTeleopCommand;
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        mJoystick = new Joystick(kJoystickID);
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
-  }
+        mExampleSubsystem = new ExampleSubsystem();
+        mExampleCommands = new ExampleCommands(mExampleSubsystem, mJoystick);
+
+        mAutoCommand = null;
+        mTeleopCommand = mExampleCommands.new ExampleCommand();
+
+        // Configure the button bindings
+        configureButtonBindings();
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+     * it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {}
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return mAutoCommand;
+    }
+
+    public Command getTeleopCommand() {
+        return mTeleopCommand;
+    }
 }
